73f733f80ca4ec16da6ba235a05ebb31eff6c660
Zach Trayner <ztrayner@gmail.com>
2018-02-22 23:16:32 -0700

    netlify'd the email sub form

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,8 +32,8 @@ export default class IndexPage extends React.Component {
                     <div className="container">
                     <h1 style={{paddingTop: '4rem'}}>The best products for your baby</h1>
                         <p>Subscribe to our weekly newsletter to get the best baby products in your inbox</p>
-                        <form className="email-registration">
-                            <input className="email-input" type="email" placeholder="Email address" />
+                        <form className="email-registration" name="email-subscribe" netlify>
+                            <input className="email-input" name="email" type="email" placeholder="Email address" />
                             <button type="submit" className="button filled">Submit</button>
                         </form>
                     </div>
